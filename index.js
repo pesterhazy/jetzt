4ecad2812203503d1a8bbe444e92ae245f6204cb
Paulus Esterhazy <pesterhazy@gmail.com>
2017-06-25 14:47:42 +0200

    WIP

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-module.exports = () => 'Ahoy, world!';
+const sleep = require('then-sleep');
+const pgp = require('pg-promise')();
+const db = pgp(process.env.JETZT_PG_CONN);
+
+module.exports = async function(req, res) {
+    const articles = await db.any('SELECT * FROM article');
+
+    return JSON.stringify(articles);
+}
